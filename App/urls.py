17e406e7b3745914caed9ca4bdbc359de17d7de9
prakashVishwakarma <prksvsvkrma@gmail.com>
2024-05-12 12:04:52 +0530

    feat: created post api

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('app/posts/', views.create_post, name='create_post'),
-    path('posts/', views.get_all_posts, name='all_posts'),
-    path('posts/<int:post_id>/', views.get_post_by_id, name='post_detail'),
+    path('app/posts/', views.create_post, name='create_post'),
 ]
