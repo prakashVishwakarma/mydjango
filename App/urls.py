3a3def87273faa7af3048d7ae394c71da80ecb7a
prakashVishwakarma <prksvsvkrma@gmail.com>
2024-05-12 12:19:57 +0530

    FEAT: delete by id api

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('app/posts/', views.create_post, name='create_post'),
+    path('app/posts/', views.create_post, name='create_post'),
     path('posts/', views.get_all_posts, name='all_posts'),
     path('posts/<int:post_id>/', views.get_post_by_id, name='post_detail'),
+    path('delete_posts/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
